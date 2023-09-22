53e60c4a4b92eea5a21a7fab64fd180499aae8be
drew-chidi <chidi.andrew@gmail.com>
2023-09-22 19:23:31 +0100

    footer mobile responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,29 +7,40 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className='px-20'>
-      <p>
-        Getlinked Tech Hackathon is a technology innovation program established
-        by a group of organizations with the aim of showcasing young and
-        talented individuals in the field of technology
-      </p>
-      <div>
+    <div className='px-20 py-[50px]'>
+      <div className=''>
+        {/* Company Logo */}
+        <div className='mb-3'>
+          <Link className={'text-white font-bold text-xl '} to={'/'}>
+            get<span className={'text-[#D434FE] '}>linked</span>
+          </Link>
+        </div>
+        <p className='text-xs leading-[172.4%]'>
+          Getlinked Tech Hackathon is a technology innovation program
+          established by a group of organizations with the aim of showcasing
+          young and talented individuals in the field of technology
+        </p>
+      </div>
+      <div className='flex gap-2 items-center mt-10 mb-8'>
         <p>Terms of Use</p>
+        <div className='h-4 w-[2px] bg-purple-50'></div>
         <p>Privacy Policy</p>
       </div>
 
       {/* Useful Links */}
-      <div>
-        <h2>Useful Links</h2>
-        <ul>
+      <div className='mb-[18px]'>
+        <h2 className='text-purple-50 mb-1.5 text-sm font-semibold'>
+          Useful Links
+        </h2>
+        <ul className='flex flex-col gap-3 text-xs'>
           <li>Overview</li>
           <li>Timeline</li>
           <li>FAQs</li>
           <li>Register</li>
         </ul>
       </div>
-      <div className='flex items-center gap-4'>
-        <h3>Follow us</h3>
+      <div className='flex items-center gap-4 mb-[61px]'>
+        <h3 className='text-xs text-purple-50'>Follow us</h3>
         <GrInstagram />
         <FaXTwitter color='white' />
         <FaFacebookF />
@@ -37,12 +48,14 @@ const Footer = () => {
       </div>
       {/* Contact us */}
       <div>
-        <h2>Contact Us</h2>
-        <div>
+        <h2 className='text-purple-50 mb-2 text-sm font-semibold'>
+          Contact Us
+        </h2>
+        <div className='flex items-start gap-4 mb-[26px]'>
           <BiSolidPhoneCall />
           <Link to='tel:+23467981819'>+234 679 81819</Link>
         </div>
-        <div>
+        <div className='flex items-start gap-4'>
           <RiMapPin2Fill />
           <Link href='#'>
             {/* <Link href='#' onClick={openMapModal}> */}
@@ -54,7 +67,7 @@ const Footer = () => {
           </Link>
         </div>
       </div>
-      <div className='flex items-center gap-2'>
+      <div className='flex items-center gap-2 mt-[60px] text-xs justify-center'>
         <p>All rights reserved.</p>
         <div className='flex items-center gap-2'>
           <FaRegCopyright />
--- src/components/Home/PrivacyPolicy.jsx
+++ src/components/Home/PrivacyPolicy.jsx
@@ -1,57 +1,74 @@
 import { BiSolidCheckCircle } from 'react-icons/bi';
 import Button from '../Button/Button';
+import License1 from '@/assets/images/license-bg.png';
+import License2 from '@/assets/images/license-key.png';
 
 const PrivacyPolicy = () => {
   return (
-    <div>
-      <div className=''>
-        <div className='flex justify-center gap-2.5 lg:gap-4 flex-col items-center text-center mt-[58px]'>
-          <h2 className='text-xl font-bold leading-[26.6px] tab:text-[2rem]'>
-            <span>Privacy Policy and</span>
-            <br />
-            <span className='text-purple-50'>Terms</span>
-          </h2>
-          <p className='text-sm text-[rgb(255_255_255/0.75)]'>
-            Last updated on September 12, 2023
-          </p>
-        </div>
-        {/* Policies */}
-        <div className='text-center mt-7'>
-          <p className='text-sm mb-[30px] tab:mb-16'>
-            Below are our privacy & policy, which outline a lot of goodies. it’s
-            our aim to always take of our participant
-          </p>
-          <div className='border border-purple-50 rounded-[5px] px-3.5 tab:px-10 xl:px-[72px] py-11 tab:py-14 xl:py-[59px]'>
-            <p>
-              At getlinked tech Hackathon 1.0, we value your privacy and are
-              committed to protecting your personal information. This Privacy
-              Policy outlines how we collect, use, disclose, and safeguard your
-              data when you participate in our tech hackathon event. By
-              participating in our event, you consent to the practices described
-              in this policy.
+    <div className='pb-[83px]'>
+      <div className='tab:flex tab:gap-12 items-center'>
+        <div className='w-1/2'>
+          <div className='flex justify-center gap-2.5 tab:gap-4 flex-col items-center text-center tab:items-start tab:text-start'>
+            <h2 className='text-xl font-bold leading-[26.6px] tab:text-[2rem]'>
+              <span>Privacy Policy and</span>
+              <br />
+              <span className='text-purple-50'>Terms</span>
+            </h2>
+            <p className='text-sm text-[rgb(255_255_255/0.75)]'>
+              Last updated on September 12, 2023
             </p>
-            <div className='flex flex-col gap-6'>
-              <div>
-                <h3>Licensing Policy</h3>
-                <p>Here are terms of our Standard License:</p>
+          </div>
+          {/* Policies */}
+          <div className='text-center mt-7 tab:text-left'>
+            <p className='text-sm mb-[30px] tab:mb-16'>
+              Below are our privacy & policy, which outline a lot of goodies.
+              it’s our aim to always take of our participant
+            </p>
+            <div className='border border-purple-50 rounded-[5px] px-3.5 tab:px-10 xl:px-[72px] py-11 tab:py-14 xl:py-[59px]'>
+              <p className='mb-5 tab:mb-6 text-xs tab:text-sm'>
+                At getlinked tech Hackathon 1.0, we value your privacy and are
+                committed to protecting your personal information. This Privacy
+                Policy outlines how we collect, use, disclose, and safeguard
+                your data when you participate in our tech hackathon event. By
+                participating in our event, you consent to the practices
+                described in this policy.
+              </p>
+              <div className='flex flex-col gap-6 mb-10 text-left'>
+                <div>
+                  <h3 className='text-[0.8125rem] text-purple-50 font-bold'>
+                    Licensing Policy
+                  </h3>
+                  <p className='text-xs font-bold'>
+                    Here are terms of our Standard License:
+                  </p>
+                </div>
+                <div className='flex gap-3.5'>
+                  <BiSolidCheckCircle color='#2DE100' size={17} />
+                  <p>
+                    The Standard License grants you a non-exclusive right to
+                    navigate and register for our event
+                  </p>
+                </div>
+                <div className='flex gap-3.5'>
+                  <BiSolidCheckCircle color='#2DE100' size={17} />
+                  <p>
+                    You are licensed to use the item available at any free
+                    source sites, for your project developement
+                  </p>
+                </div>
               </div>
-              <div>
-                <BiSolidCheckCircle color='#2DE100' size={17} />
-                <p>
-                  The Standard License grants you a non-exclusive right to
-                  navigate and register for our event
-                </p>
-              </div>
-              <div>
-                <BiSolidCheckCircle color='#2DE100' size={17} />
-                <p>
-                  You are licensed to use the item available at any free source
-                  sites, for your project developement
-                </p>
+              <div className='text-center'>
+                <Button>Read More</Button>
               </div>
             </div>
-            {/* Button Here */}
-            <Button>Read More</Button>
+          </div>
+        </div>
+        <div className='relative mt-36 tab:mt-0 mx-auto w-1/2'>
+          <div className='absolute -top-24 max-w-[249px] mx-auto tab:max-w-[530px]'>
+            <img src={License1} alt='' />
+          </div>
+          <div className='max-w-[262px] tab:max-w-[559px]'>
+            <img src={License2} alt='license' />
           </div>
         </div>
       </div>
